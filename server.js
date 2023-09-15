b06c860623140fad62e402ff5bb0eca20364028c
Jhordan Kendy Suzuki <jhordan.suzuki@followize.local>
2023-09-15 10:36:42 -0300

    server local

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { log } = require('console')
 const express = require('express')
 const http = require('http')
 const WebSocket = require('ws')
@@ -12,6 +13,8 @@ const APP_URL = process.env.URL || `http://localhost:${APP_PORT}`
 app.use('/public', express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 app.get('/admin', (req, res) => res.sendFile(__dirname + '/public/admin.html'))
+
+server.listen(3000,()=>console.log('servidor rodando'))
 
 // const express = require("express");
 // const http = require("http");
