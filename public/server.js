137aa619ce2c0b4136ff5e0454315e251904c319
Jhordan Kendy Suzuki <jhordan.suzuki@followize.local>
2023-09-15 10:24:12 -0300

    first commit

---
+++ public/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
+
+const app = express()
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+const APP_PORT = process.env.PORT || 3000
+const APP_URL = process.env.URL || `http://localhost:${APP_PORT}`
+
+app.use('/public', express.static('public'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
+app.get('/admin', (req, res) => res.sendFile(__dirname + '/public/admin.html'))
+
+// const express = require("express");
+// const http = require("http");
+// const WebSocket = require("ws");
+
+// const app = express();
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+
+// const APP_PORT = process.env.PORT || 3000;
+// const APP_URL = process.env.URL || `http://localhost:${APP_PORT}`;
+
+// const ACTIONS = {
+//   ADMIN: "admin",
+//   DRAW: "draw",
+//   CLIENT_COUNT_UPDATE: "clientCountUpdate",
+// };
+
+// app.use("/public", express.static("public"));
+// app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
+// app.get("/admin", (req, res) => res.sendFile(__dirname + "/public/admin.html"));
